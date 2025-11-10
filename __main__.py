41b92afa7cdfa5e4f0c4ec219305e9b60f79d502
Vero <veronika.netch@gmail.com>
2025-11-10 20:40:11 +0100

    Calibration WIP

--- __main__.py
+++ __main__.py
@@ -1,5 +1,6 @@
 import task1_camera_calibration
 
 if __name__ == '__main__':
-    camera_calibrator = task1_camera_calibration.CameraCalibration()
-    camera_calibrator.run_detection_check("./calibration/")
+    camera_calibrator = task1_camera_calibration.CameraCalibration((16, 22), (1440, 960), 30)
+    camera_calibrator.set_directory("./calibration/")
+    camera_calibrator.run_detection_check()
--- task1_camera_calibration.py
+++ task1_camera_calibration.py
@@ -1,17 +1,31 @@
 import cv2
 import os
+import numpy as np
 
 class CameraCalibration:
-    def __init__(self):
+    def __init__(self, board_size: tuple, image_size: tuple, checker_size: int):
         self.dictionary = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_250)
         self.parameters = cv2.aruco.DetectorParameters()
         self.detector = cv2.aruco.ArucoDetector(self.dictionary, self.parameters)
+        self.board_size = board_size
+        self.image_size = image_size
+        self.pattern_size = (board_size[0]-1, board_size[1]-1)
+        self.checker_size = checker_size
+        self.directory = ""
 
-    def run_detection_check(self, directory):
-        for file in os.listdir(directory):
+    def set_directory(self, directory: str):
+        self.directory = directory
+
+    def generate_object_points(self):
+        points = np.zeros((self.pattern_size[0] * self.pattern_size[1], 3), np.float32)
+        points[:, :2] = np.mgrid[:self.pattern_size[0], :self.pattern_size[1]].T.reshape(-1, 2)
+        return points
+
+    def run_detection_check(self):
+        for file in os.listdir(self.directory):
             filename = os.fsdecode(file)
             print(filename)
-            image = cv2.imread(directory + filename)
+            image = cv2.imread(self.directory + filename)
             cv2.imshow(filename, image)
             cv2.waitKey(0)
 
@@ -21,13 +35,45 @@ class CameraCalibration:
             cv2.imshow(filename, detected)
             cv2.waitKey(0)
 
-    def calibrate(self, directory):
+    def detect_corners(self):
         all_corners = []
         all_ids = []
-        for file in os.listdir(directory):
+        for file in os.listdir(self.directory):
             filename = os.fsdecode(file)
-            image = cv2.imread(directory + filename)
+            image = cv2.imread(self.directory + filename)
             corners, ids, _ = self.detector.detectMarkers(image)
             all_corners.append(corners)
             all_ids.append(ids)
         return all_corners, all_ids
+
+    def calibrate(self):
+        corners, ids = self.detect_corners()
+        n = len(corners)
+        object_points = self.generate_object_points()
+
+        _, camera_matrix, distortion_coefficients, _, _ = cv2.calibrateCamera(
+                objectPoints=np.array([object_points] * n),
+                imagePoints=corners,
+                imageSize=self.image_size,
+                cameraMatrix=None,
+                distCoeffs=None)
+        return camera_matrix, distortion_coefficients
+
+    def run_undistortion_check(self, image_to_undistort):
+        camera_matrix, distortion_coefficients = self.calibrate()
+        new_camera_matrix, roi = cv2.getOptimalNewCameraMatrix(camera_matrix, distortion_coefficients, self.image_size, 0)
+        print(new_camera_matrix)
+        map1, map2 = cv2.initUndistortRectifyMap(camera_matrix,
+                                                 distortion_coefficients,
+                                                 np.eye(3),
+                                                 new_camera_matrix,
+                                                 size=self.image_size,
+                                                 m1type=cv2.CV_32FC1)
+        x, y, w, h = roi
+        print("roi:", roi)
+        image_undistorted = image_to_undistort.copy()
+        image_undistorted = cv2.remap(image_to_undistort, map1, map2, cv2.INTER_LINEAR)
+        chess_cropped = chess_undistorted[y:y + h, x:x + w]
+        cv2.imshow("chessboard", chess_cropped)
+        cv2.waitKey(0)
+        return map1, map2
